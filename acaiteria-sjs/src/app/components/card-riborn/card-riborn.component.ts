6044af72bc4a1de6cb48ccdfe40e71d8ef97ab85
neto-b2c <clodoaldo.neto.116@ufrn.edu.br>
2022-03-15 22:06:32 -0300

    added media query on home page

--- acaiteria-sjs/src/app/components/card-riborn/card-riborn.component.ts
+++ acaiteria-sjs/src/app/components/card-riborn/card-riborn.component.ts
@@ -3,6 +3,8 @@ import { Component, Input, OnInit } from '@angular/core';
 /**
 * @description é um componente de cartão pesonalizado, possui como Inputs.
 *
+* @param {string} widthth @Input a largura.
+* @param {string} heigth @Input a altura do card.
 * @param {string} cor @Input o cor do realce ao titulo do cartção.
 * @param {string} title @Input o titulo do cartão.
 * @param {string} content @Input um texto introdutorio
@@ -20,9 +22,21 @@ export class CardRibornComponent implements OnInit {
   @Input() public content:string="";
   @Input() public profile:string="";
 
+  @Input() public cardContWidth:string="230px";
+  @Input() public cardContHeight:string="315px";
+  public corpoCardWidth:string="";
+  public corpoCardHeight:string="";
+
   constructor() { }
 
   ngOnInit(): void {
+    this.corpoCardWidth = this.calcTamnhoCard(this.cardContWidth);
+    this.corpoCardHeight = this.calcTamnhoCard(this.cardContHeight);
+  }
+  public calcTamnhoCard(n:string):string{
+    const x = n.split('p');
+    const y = parseInt(x[0]);
+    return (y-50)+"px";
   }
 
 }
